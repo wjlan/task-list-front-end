f9905f96af0eb25c343048e4c0d67f1205f06a7f
wanjunlan <wanjunlan@gmail.com>
2022-12-16 11:54:31 -0800

    wave2 in progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
@@ -16,13 +16,28 @@ const TASKS = [
 ];
 
 const App = () => {
+  const initialCopy = TASKS.map(task => {
+    return {...task};
+  });
+
+  const [taskData, setTaskData] = useState(initialCopy);
+
+  const updateIsComplete = (taskId) => {
+    console.log("calling updateIsComplete");
+  }
+
+
+ 
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          {<TaskList tasks={initialCopy} updateIsComplete={updateIsComplete} />}
+        </div>
       </main>
     </div>
   );
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Task from './Task';
 import './TaskList.css';
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({tasks, updateIsComplete}) => {
   const getTaskListJSX = (tasks) => {
     return tasks.map((task) => {
       return (
@@ -12,6 +12,7 @@ const TaskList = ({ tasks }) => {
           id={task.id}
           title={task.title}
           isComplete={task.isComplete}
+          updateIsComplete={updateIsComplete}
         />
       );
     });
@@ -27,6 +28,7 @@ TaskList.propTypes = {
       isComplete: PropTypes.bool.isRequired,
     })
   ).isRequired,
+  updateIsComplete: PropTypes.func.isRequired
 };
 
 export default TaskList;
